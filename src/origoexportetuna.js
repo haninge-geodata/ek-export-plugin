a3c5bfa16a9648dc6bc3630189d5ecfe541df0eb
Edvin Fackel <70571631+sweco-sefaed@users.noreply.github.com>
2021-02-22 15:06:26 +0100

    Using MutationObserver (#2)
    Instead of listening to deprecated event 'DOMSubtreeModified'

--- src/origoexportetuna.js
+++ src/origoexportetuna.js
@@ -10,25 +10,20 @@ const Origoexportetuna = function Origoexportetuna(options = {}) {
   const attribute = Object.prototype.hasOwnProperty.call(options, 'attribute') ? options.attribute : null;
   const buttonText = Object.prototype.hasOwnProperty.call(options, 'buttonText') ? options.buttonText : null;
 
-  let globalURL;
-
   function dotNetUrlBuilder(items) {
-    let fnrString = '';
+    let idString = '';
     items.forEach((item) => {
-      fnrString += `${item.getFeature().get(attribute)};`;
+      idString += `${item.getFeature().get(attribute)};`;
     });
-    return `${hostname}/search/estateExcelReport.aspx?searchstring=&estateLayer=${layer}&ids=${fnrString}&functionParam=excel`;
+    return `${hostname}/search/estateExcelReport.aspx?searchstring=&estateLayer=${layer}&ids=${idString}&functionParam=excel`;
   }
 
-  function evtListener() {
+  function listenerFunc() {
     const buttonContainer = document.getElementsByClassName('export-buttons-container')[0];
-    const buttonNeedsToBeAdded = document.getElementById('callDotNet') === null;
 
     if (typeof buttonContainer !== 'undefined') {
-      const items = selectionManager.getSelectedItemsForASelectionGroup(layer);
+      const buttonNeedsToBeAdded = document.getElementById('callDotNet') === null;
       const hasOrigoExportButton = buttonContainer.getElementsByTagName('div').length === 1;
-
-      globalURL = dotNetUrlBuilder(items);
 
       if (hasOrigoExportButton) {
         buttonContainer.getElementsByTagName('div')[0].style.display = 'inline-block';
@@ -37,7 +32,7 @@ const Origoexportetuna = function Origoexportetuna(options = {}) {
       if (buttonNeedsToBeAdded) {
         const button = dom.html(`<button id="callDotNet" class="export-button" style="margin-left: 0.5rem;">${buttonText}</button>`);
         buttonContainer.appendChild(button);
-        document.getElementById('callDotNet').addEventListener('click', () => window.open(globalURL, '_blank'));
+        document.getElementById('callDotNet').addEventListener('click', () => window.open(dotNetUrlBuilder(selectionManager.getSelectedItemsForASelectionGroup(layer)), '_blank'));
       }
     }
   }
@@ -48,7 +43,16 @@ const Origoexportetuna = function Origoexportetuna(options = {}) {
       if (layer && hostname && attribute && buttonText) {
         viewer = evt.target;
         selectionManager = viewer.getSelectionManager();
-        document.getElementById('sidebarcontainer').addEventListener('DOMSubtreeModified', evtListener);
+
+        const observer = new MutationObserver(() => {
+          const el = document.getElementsByClassName('listcontainer')[0];
+          if (el && el.offsetParent !== null) {
+            listenerFunc();
+            document.getElementsByClassName('urvalelement')[0].addEventListener('click', listenerFunc);
+          }
+        });
+
+        observer.observe(document.querySelector('#sidebarcontainer'), { attributes: true, childList: true, characterData: true });
       } else {
         alert('Kunde inte ladda plugin Origoexportetuna.\nParametrar saknas.');
       }
