5dc0929cb23447d18eb8f895bec92c669c576931
Grammostola <perssond9@gmail.com>
2022-11-29 17:03:33 +0100

    deps: remaining webpack task file affected by deps update

--- tasks/webpack.dev.js
+++ tasks/webpack.dev.js
@@ -1,6 +1,5 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = merge(common, {
   output: {
@@ -14,11 +13,16 @@ module.exports = merge(common, {
   mode: 'development',
   module: {
   },
-  plugins: [
-    new WriteFilePlugin()
-  ],
   devServer: {
-    contentBase: './',
-    port: 9009
+    static: './',
+    port: 9008,
+    devMiddleware: {
+      //index: true,
+      //mimeTypes: { 'text/html': ['phtml'] },
+      //publicPath: '/publicPathForDevServe',
+      //serverSideRender: true,
+      writeToDisk: true
+    },
+    
   }
 });
